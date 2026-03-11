b2c35d6167aec1db197ad1367b13b8cfbd82e303
fawwazgoreng <hatorimikael@gmail.com>
2026-03-11 20:44:39 +0700

    add docker file and docker-compose

--- app/redis.js
+++ app/redis.js
@@ -1,8 +1,10 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
+    host: process.env.REDIS_HOST || 'redis',
     port: 6379,
-   keyPrefix: "url-shortener:" 
+    keyPrefix: "url-shortener:",
+    db: 0
 });
 
 export default redis;
